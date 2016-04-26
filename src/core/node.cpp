e89971b00d5d5ea97e0f72cdadfc8f9431f4df5a
Arash <arash@localhost.localdomain>
2016-04-26 15:12:19 +0430

    second commit

---
+++ src/core/node.cpp
@@ -0,0 +1,14 @@
+#include "core/node.h"
+
+using namespace nodesys;
+using namespace core;
+
+Node::Node() {
+  std::cout << "Hi!" << std::endl;
+  
+}
+
+
+Node::~Node() {
+  std::cout << "Bye!" << std::endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+// Copyright 2016 Arash Arj
+
+#include <iostream>
+#include "core/node.cpp"
+
+int main(void) {
+  nodesys::core::Node my_node;
+  return 0;
+}
